87951997cdb4550a4f48abd9ccde8166fea3e2ea
Emilse Arias <emilsearias@gmail.com>
2023-04-03 10:13:50 -0300

    agregamos un ejemplo de setTimeout y setInterval

--- js/app.js
+++ js/app.js
@@ -4,8 +4,21 @@ let contador = 1;
 
 function saludar(){
     document.write(`<p>Hola mundo, cantidad de veces ${contador}</p>`)
+    if(contador === 5){
+        clearInterval(identificador)
+    }
+    contador++;
 }
 
+// window.setTimeout(saludar, 3000);
 
+// setTimeout(saludar, 3000);
 
-//setInterval
+// setTimeout(()=>{
+//     document.write(`<p>Hola mundo, cantidad de veces ${contador}</p>`)
+// }, 4000);
+
+//setInterval -> repite el llamado de una funcion cada x milisegundos
+
+ let identificador = setInterval(saludar, 2000)
+
