be5cf91c2284633806605e160a6ad1bc8ec25ed6
Emilse Arias <emilsearias@gmail.com>
2023-04-03 09:53:27 -0300

    agregamos la estructura inicial y comence con el archivo app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//setTimeout -> ejecuta una funcion pasado cierto tiempo
+
+let contador = 1;
+
+function saludar(){
+    document.write(`<p>Hola mundo, cantidad de veces ${contador}</p>`)
+}
+
+
+
+//setInterval
